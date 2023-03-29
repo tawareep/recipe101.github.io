765d08ce9adb23733c6ad9b5d9533638312dcff1
tawareep <129275332+tawareep@users.noreply.github.com>
2023-03-29 17:13:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+//ไม่ได้ใช้แล้ว
+
+function myFunction() {
+  var x = document.getElementById("show");
+  if (x.innerHTML === "Show Ingredients") {
+    x.innerHTML = "Hide Ingredients";
+  } else {
+    x.innerHTML = "Show Ingredients";
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("ingre");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
